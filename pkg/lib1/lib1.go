62abf7a6cc50eee674094fa681c5af8ab61aad4c
eupcliff <74814130+eupcliff@users.noreply.github.com>
2024-06-09 11:59:43 +0800

    Create lib1.go

---
+++ pkg/lib1/lib1.go
@@ -0,0 +1,179 @@
+package lib1
+
+import (
+	"log"
+	"fmt"
+)
+
+func Lib1_hello () {
+	log.Printf("Lib1_hello!")
+}
+
+func Lib1_Add (a int, b int) int {
+	return (a+b)
+}
+
+func Lib1_twoSum (nums []int, target int) []int {
+	for i, v0 := range nums[0:] {
+		for j, v1 := range nums[i+1:] {
+			if v0 + v1 == target {
+				return []int{i, i+j+1}
+			}
+		}
+	}
+	return []int{0, 0}
+}
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func (l *ListNode) Append (val int) {
+	for p := l; ; p = p.Next {
+		if p.Next == nil {
+			p.Next = new(ListNode)
+			p.Next.Val = val
+			return
+		}
+	}
+}
+
+func (l *ListNode) Dump () {
+	i := 0
+	str := ""
+	for p := l; ; p = p.Next{
+		str += fmt.Sprintf("%d ", p.Val)
+//		log.Printf("list[%d]=%d\n", i, p.Val)
+		i++
+		if p.Next == nil {
+			break
+		}
+	}
+	log.Printf("%s", str)
+}
+
+func (l *ListNode) append2 (val int) *ListNode {
+	if l.Val < 0 {
+		l.Val = val
+		return l
+	} else {
+		var p = new(ListNode)
+		p.Val = val
+		p.Next = l
+		return p
+	}
+}
+
+type ListNodeD struct {
+    p1 *ListNode
+    p2 *ListNode
+    next *ListNodeD
+    prev *ListNodeD
+}
+
+func (l *ListNodeD) myappend (prev *ListNodeD, p1 *ListNode, p2 *ListNode) *ListNodeD {
+	var next *ListNodeD
+	if l.p1 == nil && l.p2 == nil {
+		// is head
+		l.p1 = p1
+		l.p2 = p2
+		//l.prev = prev
+		next = l
+	} else {
+		l.next = new(ListNodeD)
+		l.next.p1 = p1
+		l.next.p2 = p2
+		l.next.prev = prev
+		next = l.next
+	}
+	return next
+}
+
+func (l *ListNodeD) print_all () {
+	for p := l; p.next != nil ; p = p.next {
+		str := ""
+		if p.p1 == nil {
+			str += "v1: ,"
+		} else {
+			str += fmt.Sprintf("v1:%d ,", p.p1.Val)
+		}
+		if p.p2 == nil {
+			str += "v2: "
+		} else {
+			str += fmt.Sprintf("v2:%d ", p.p2.Val)
+		}
+		//log.Printf("val=(%s), p1=%p, p2=%p, curr=%p, next=%p, prev=%p", str, p.p1, p.p2, p, p.next, p.prev)
+		log.Printf("val=(%s)", str)
+	}
+}
+
+func (l *ListNodeD) print_all_reverse () {
+	log.Println("print_all_reverse: ")
+	for p := l;  ; p = p.prev {
+		str := ""
+		if p.p1 == nil {
+			str += "v1: ,"
+		} else {
+			str += fmt.Sprintf("v1:%d ,", p.p1.Val)
+		}
+		if p.p2 == nil {
+			str += "v2: "
+		} else {
+			str += fmt.Sprintf("v2:%d ", p.p2.Val)
+		}
+		log.Printf("val=(%s), p1=%p, p2=%p, curr=%p, next=%p, prev=%p", str, p.p1, p.p2, p, p.next, p.prev)
+		if p.prev == nil {
+			break
+		}
+	}
+}
+
+func Lib1_addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var l3 = new(ListNodeD)
+	p1 := l1
+	p2 := l2
+	var tail_l1 *ListNodeD
+	var tail_l2 *ListNodeD
+	p3 := l3
+	for {
+		p3 = p3.myappend(p3, p1, p2)
+		if p1 != nil {
+			tail_l1 = p3
+			p1 = p1.Next
+		}
+		if p2 != nil {
+			tail_l2 = p3
+			p2 = p2.Next
+		}
+		if p1 == nil && p2 == nil {
+			p3.myappend(p3, p1, p2)
+			break
+		}
+	}
+	r1 := tail_l1
+	r2 := tail_l2
+	var z = new(ListNode)
+	z.Val = -1
+	second := 0
+	first := 0
+	for {
+		if r1 == nil && r2 == nil {
+			break
+		}
+		sum := second
+		if r1 != nil {
+			sum += r1.p1.Val
+			r1 = r1.prev
+		}
+		if r2 != nil {
+			sum += r2.p2.Val
+			r2 = r2.prev
+		}
+		first = (sum%10)
+		second = (sum/10)
+		z = z.append2(first)
+	}
+	z.Dump()
+	return nil
+}
